e1a15310bd4fd66c17a43d59eb6ca90bff00e029
Paul Coroneos <paul@pcoroneos.com>
2020-04-16 16:50:55 -0500

    add menu item styles

--- src/components/menu-item/menu-item.tsx
+++ src/components/menu-item/menu-item.tsx
@@ -8,9 +8,15 @@ interface MenuItemProps {
 }
 
 export const MenuItem: React.FC<MenuItemProps> = ({ title, imageUrl, size }) => (
-    <div style={{ backgroundImage: `url(${imageUrl})` }} className={`menu-item ${size}`}>
+    <div className={`${size} menu-item`}>
+        <div
+            className="background-image"
+            style={{
+                backgroundImage: `url(${imageUrl})`,
+            }}
+        />
         <div className="content">
-            <h1 className="title">{title}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">SHOP NOW</span>
         </div>
     </div>

a413dc85d379c508a432025f5c17d82a344b8ede
Paul Coroneos <paul@pcoroneos.com>
2020-04-16 23:13:46 -0500

    complete header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import './App.scss';
 import { HomePage } from './pages/homepage/homepage';
 import { ShopPage } from './pages/shop/shop';
 import { Switch, Route } from 'react-router-dom';
+import { Header } from './components/header/header';
 
 const App = () => {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />

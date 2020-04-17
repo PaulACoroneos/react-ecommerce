d9eee1b4c95eb06ce57bf8bdd86734edced397d1
Paul Coroneos <paul@pcoroneos.com>
2020-04-17 12:52:19 -0500

    complete initial signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.scss';
 import { HomePage } from './pages/homepage/homepage';
 import { ShopPage } from './pages/shop/shop';
-import { Switch, Route } from 'react-router-dom';
+import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import { Header } from './components/header/header';
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/shop" component={ShopPage} />
+                <Route path="/signin" component={SignInAndSignUpPage} />
             </Switch>
         </div>
     );

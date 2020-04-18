59b9db63939bcefcd85f3e580b9a58c10759864c
Paul Coroneos <paul@pcoroneos.com>
2020-04-18 15:48:22 -0500

    implemented firebase utils, and store authenticated users into firestore db

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase, { User } from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
@@ -12,11 +12,29 @@ const firebaseConfig = {
     appId: '1:285707271970:web:be4462687a6a4c99436f7b',
     measurementId: 'G-LRVPZNJVKX',
 };
-
 firebase.initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
+
+export const createUserProfileDocument = async (userAuth: User | null, additionalData: any) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({ displayName, email, createdAt, ...additionalData });
+        } catch (error) {
+            console.error('error creating user', error.message);
+        }
+    }
+    return userRef;
+};
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.tsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
 import { SignIn } from '../../components/sign-in/sign-in';
+import { SignUp } from '../../components/sign-up/sign-up';
 
 export const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 );

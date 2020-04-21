0f8a8bd10a32d00e219ae7dbd5617b09077c41be
Paul Coroneos <paul@pcoroneos.com>
2020-04-20 23:48:15 -0500

    finish dropdown

--- src/components/cart-icon/cart-icon.tsx
+++ src/components/cart-icon/cart-icon.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { toggleCartVisibility } from '../../store/cart/cart-slice';
+import { RootState } from '../../store/reducers/reducers';
 
 export const CartIcon = () => {
     const dispatch = useDispatch();
+    const cartItemQty = useSelector((state: RootState) =>
+        state.cart.cartItems.reduce((qty, item) => qty + (item?.quantity || 0), 0),
+    );
     return (
         <div className="cart-icon" onClick={() => dispatch(toggleCartVisibility())}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{cartItemQty}</span>
         </div>
     );
 };
